a49352b0a135866d95145c40753c08fbdc52d53a
Dylan Violo <violody@sheridancollege.ca>
2025-01-15 13:58:11 -0500

    added setter/getter for studentID

---
+++ src/studentlist/Student.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author fagun at 12/05/2024
+ */
+public class Student {
+    private String name;
+    private String studentID;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getStudentID() {
+        return studentID;
+    }
+     public void setStudentID(String studentID) {
+         this.studentID = studentID;
+     }
+   
+}
