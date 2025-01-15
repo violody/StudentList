858857f402e457020dd038f531add2e97a214ee7
Dylan Violo <violody@sheridancollege.ca>
2025-01-15 14:18:34 -0500

    added another student to array

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -15,19 +15,20 @@ public class StudentList {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Student[] studentList = new Student[3];//change size before running
+        Student[] studentList = new Student[4];//change size before running
         Student s1 = new Student();
         s1.setName("Fagun Vankawala");
         Student s2 = new Student();
         s2.setName("Palak Shah");
         Student s3 = new Student();
         s3.setName("Megh Vankawala");
-//        Student s4 = new Student();
-//        s4.setName("Megh Vankawala");
+        Student s4 = new Student();
+        s4.setName("Megh Vankawala");
 //       
         studentList[0] = s1;
         studentList[1] = s2;
         studentList[2] = s3;
+        studentList[3] = s4;
         
         //fill in 4 student names before running
         for (Student s : studentList) {
